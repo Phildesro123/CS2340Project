3f681ac280a8214263288f4de0ae8d557196fd9d
Ski <ski.ingurgio@gatech.edu>
2019-09-20 13:23:28 -0400

    Update Game.java

--- src/models/Game.java
+++ src/models/Game.java
@@ -18,28 +18,175 @@ public class Game {
 	}
 
 	//main method shows the welcome screen?
-
+	//display startButton
 	/*if(startButton.isPressed()) {
-		screen = configScreen; //????
+		//change to Config screen
 	}
 
-	String tempName = nameBox.output(); //can be blank
-	//later on the confirm selection button will update game.player.setName()
+	//Config screen
+		//display nameBox (text input)
+		String tempName = nameBox.output(); //can be blank
+		//maybe implement using a "set" button to set it to player.setName()?
+			//would need a way to keep name despite changing difficulty
+		//later on the confirm selection button will update game.player.setName()
 
-	//based on which button is pressed, the Game object is created, which creates the appropriate player
-	Game game;
-	if(easyButton.isPressed()) {
-		game = new Game(16, tempName);
-	} 
-	if(medButton.isPressed()) {
-		game = new Game(12, tempName);
-	}
-	if(hardButton.isPressed()) {
-		game = new Game(8, tempName);
-	}
+		//based on which button is pressed, the Game object is created, which creates the appropriate player
+		Game game;
+		Player player = new Player("", 0, 0.0);
 
+		//display difficuly as blank
 
+		//display easyButton
+		//display medButton
+		//display hardButton
+		if(easyButton.isPressed()) {
+			game = new Game(16, tempName);
+			player = game.getPlayer();
+			//update difficulty as Easy
+			//update unallocated
+			//update pilot
+			//update fighter
+			//update merchant
+			//update engineer
+		} 
+		if(medButton.isPressed()) {
+			game = new Game(12, tempName);
+			player = game.getPlayer();
+			//update difficulty as Medium
+			//update unallocated
+			//update pilot
+			//update fighter
+			//update merchant
+			//update engineer
+		}
+		if(hardButton.isPressed()) {
+			game = new Game(8, tempName);
+			player = game.getPlayer();
+			//update difficulty as Hard
+			//update unallocated
+			//update pilot
+			//update fighter
+			//update merchant
+			//update engineer
+		}
+
+		//display "Unallocated: " + player.getSkillPoints();
+
+		//display "Pilot: " + player.getSkillSet()[0];
+		//display pilotUp
+		//display pilotDown
+		if(pilotUp.isPressed()) {
+			if(player.getSkillPoints() > 0) { //no adding if you have no points
+				player.addToSkillSet(0);
+				//update unallocated to show new skill point amount
+				//update pilot to show added point
+			}
+		}
+		if(pilotDown.isPressed()) {
+			if (player.getSkillSet()[0] > 0) { //no removing points if at 0
+				player.removeFromSkillSet(0);
+				//update unallocated to show new skill point amount
+				//update pilot to show removed point
+			}
+		}
+
+		//display "Fighter: " + player.getSkillSet()[1];
+		//display fighterUp
+		//display fighterDown
+		if(fighterUp.isPressed()) {
+			if(player.getSkillPoints() > 0) { //no adding if you have no points
+				player.addToSkillSet(1);
+				//update unallocated to show new skill point amount
+				//update fighter to show added point
+			}
+		}
+		if(fighterDown.isPressed()) {
+			if (player.getSkillSet()[1] > 0) { //no removing points if at 0
+				player.removeFromSkillSet(1);
+				//update unallocated to show new skill point amount
+				//update fighter to show removed point
+			}
+		}
+
+		//display "Merchant: " + player.getSkillSet()[2];
+		//display merchantUp
+		//display merchantDown
+		if(merchantUp.isPressed()) {
+			if(player.getSkillPoints() > 0) { //no adding if you have no points
+				player.addToSkillSet(2);
+				//update unallocated to show new skill point amount
+				//update merchant to show added point
+			}
+		}
+		if(merchantDown.isPressed()) {
+			if (player.getSkillSet()[2] > 0) { //no removing points if at 0
+				player.removeFromSkillSet(2);
+				//update unallocated to show new skill point amount
+				//update merchant to show removed point
+			}
+		}
+
+		//display "Engineer: " + player.getSkillSet()[3];
+		//display engineerUp
+		//display engineerDown
+		if(engineerUp.isPressed()) {
+			if(player.getSkillPoints() > 0) { //no adding if you have no points
+				player.addToSkillSet(3);
+				//update unallocated to show new skill point amount
+				//update engineer to show added point
+			}
+		}
+		if(engineerDown.isPressed()) {
+			if (player.getSkillSet()[3] > 0) { //no removing points if at 0
+				player.removeFromSkillSet(3);
+				//update unallocated to show new skill point amount
+				//update engineer to show removed point
+			}
+		}
+
+		//display confirmButton
+		if(confirmButton.isPressed()) {
+			if(player.getCredits() == 0.0) { //no difficulty was ever selected to override this
+				//display error message "Difficulty must be chosen"
+			} else if(tempName = "") {
+				//display error message "Name cannot be blank"
+			} else {
+				player.setName(tempName); //setName doesn't exist?
+				game.setPlayer(player);
+				//change to Build Display screen
+			}
+		}
+
+	//Build Display screen
+		//display "Name: " + game.getPlayer().getName();
+		if (game.getDifficulty() == 16) {
+			//display "Difficulty: Easy";
+		} 
+		if (game.getDifficulty() == 12) {
+			//display "Difficulty: Medium";
+		}
+		if (game.getDifficulty() == 8) {
+			//display "Difficulty: Hard";
+		}
+		//display "Pilot: " + game.getPlayer().getSkillSet()[0];
+		//display "Fighter: " + game.getPlayer().getSkillSet()[1];
+		//display "Merchant: " + game.getPlayer().getSkillSet()[2];
+		//display "Engineer: " + game.getPlayer().getSkillSet()[3];
+
+		//display "Credits: " + game.getPlayer().getCredits();
 
 	*/
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
 
 }
