2aed7ba4d67f1b8128eb260986efc850492e2046
TheGrim159 <47199533+TheGrim159@users.noreply.github.com>
2019-09-21 16:21:26 -0400

    Update app.java

--- src/app.java
+++ src/app.java
@@ -11,5 +11,6 @@ public class app {
         f.setSize(400,500);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
