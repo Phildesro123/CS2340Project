24eeb8ec3a37a55df73fa9fb1becc2cbdc2fa385
Philippe Desrosiers <phildesro@yahoo.com>
2019-09-21 18:36:55 -0400

    Added launcher file

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,7 @@
 import models.CustomizationPage;
 public class Launcher {
     public static void main(String[] args) {
+        //Will add the welcome screen soon
         new CustomizationPage();
     }
 }
