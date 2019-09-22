f27eebd030172a57e7da4e3960c1978b93bad1c4
Philippe Desrosiers <phildesro@yahoo.com>
2019-09-21 20:05:40 -0400

    Adding changing windows in launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,9 +1,11 @@
 import screens.CustomizationPage;
+import screens.Welcome;
 public class Launcher {
     public static void main(String[] args) {
         //Will add the welcome screen soon
-        CustomizationPage screen = new CustomizationPage();
-        screen.getGame()
+        Welcome welcomeScreen = new Welcome();
+        //CustomizationPage screen = new CustomizationPage();
+        //screen.getFrame().setVisible(false);
 
     }
 }
