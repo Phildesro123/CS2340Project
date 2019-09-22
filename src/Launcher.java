95fe1dfe9bc5f40ff58a99a306c40168a94cd65d
Marvin Crumbs <mcrumbs3@gatech.edu>
2019-09-22 16:58:44 -0400

    Resolving merge conflict

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,7 +3,6 @@ public class Launcher {
     public static void main(String[] args) {
         //Will add the welcome screen soon
         CustomizationPage screen = new CustomizationPage();
-        screen.getGame()
-
+        screen.getGame();
     }
 }
