b766dd0e398a493ff56c2cf01952b38587a54629
Philippe Desrosiers <phildesro@yahoo.com>
2019-10-26 15:19:40 -0400

    Added NPC parent class

---
+++ src/models/NPC.java
@@ -0,0 +1,22 @@
+package models;
+
+import javax.swing.*;
+
+public abstract class NPC {
+    ImageIcon icon;
+    String name;
+
+    /**
+     * Creates a new NPC object
+     * @param icon Icon image
+     * @param name Name of NPC
+     */
+    public NPC(ImageIcon icon, String name) {
+        this.icon = icon;
+        this.name = name;
+    }
+
+    //Any other abstract methods?
+
+
+}
