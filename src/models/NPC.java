4017688bee84cd2fc4a33b738cb9e74da8fd1826
Philippe Desrosiers <phildesro@yahoo.com>
2019-10-27 17:16:48 -0400

    added getters and setters to NPC

--- src/models/NPC.java
+++ src/models/NPC.java
@@ -3,8 +3,8 @@ package models;
 import javax.swing.*;
 
 public abstract class NPC {
-    ImageIcon icon;
-    String name;
+    private ImageIcon icon;
+    private String name;
 
     /**
      * Creates a new NPC object
@@ -13,6 +13,38 @@ public abstract class NPC {
      */
     public NPC(ImageIcon icon, String name) {
         this.icon = icon;
+        this.name = name;
+    }
+
+    /**
+     * Gets the icon of NPC
+     * @return Icon image
+     */
+    public ImageIcon getIcon() {
+        return icon;
+    }
+
+    /**
+     * Sets the icon image of NPC
+     * @param icon Icon image
+     */
+    public void setIcon(ImageIcon icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * Gets the name of NPC
+     * @return name of NPC
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set's the name of NPC
+     * @param name Name of NPC
+     */
+    public void setName(String name) {
         this.name = name;
     }
 
