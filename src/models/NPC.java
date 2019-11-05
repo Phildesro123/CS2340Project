29abfafea6eac9b12341d13db430b1b08ae4853b
Philippe Desrosiers <phildesro@yahoo.com>
2019-11-05 16:28:24 -0500

    Changed NPC to an interface

--- src/models/NPC.java
+++ src/models/NPC.java
@@ -2,53 +2,11 @@ package models;
 
 import javax.swing.*;
 
-public abstract class NPC {
-    private ImageIcon icon;
-    private String name;
+public interface NPC {
+    ImageIcon icon;
+    String name;
 
-    /**
-     * Creates a new NPC object
-     * @param icon Icon image
-     * @param name Name of NPC
-     */
-    public NPC(ImageIcon icon, String name) {
-        this.icon = icon;
-        this.name = name;
-    }
-
-    /**
-     * Gets the icon of NPC
-     * @return Icon image
-     */
-    public ImageIcon getIcon() {
-        return icon;
-    }
-
-    /**
-     * Sets the icon image of NPC
-     * @param icon Icon image
-     */
-    public void setIcon(ImageIcon icon) {
-        this.icon = icon;
-    }
-
-    /**
-     * Gets the name of NPC
-     * @return name of NPC
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Set's the name of NPC
-     * @param name Name of NPC
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    //Any other abstract methods?
+    public abstract void interact();
 
 
 }
