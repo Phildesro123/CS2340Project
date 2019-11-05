25a870dcf54a79a29860e65c6cdd022955c79489
TheGrim159 <47199533+TheGrim159@users.noreply.github.com>
2019-11-05 17:53:48 -0500

    Revert "asdjf"
    This reverts commit 5af9d01757f659def957dd0c24de678a83081491.

--- src/screens/Map.java
+++ src/screens/Map.java
@@ -21,10 +21,8 @@ public class Map {
     public Map(Game game) {
         Universe universe = game.getUniverse();
         player = game.getPlayer();
-        travel = new Travel(player.getShip(),
-                game.getDifficulty().modifier(), player.getSkillSet());
-        encounter = new Encounter(player.getShip().getCargo(),
-                player.getCredits(), game.getDifficulty().modifier());
+        travel = game.getTravel();
+        encounter = game.getEncounter();
         Region[] regions = new Region[universe.getRegions().length - 1];
         int cnt = 0;
         for (Region region: universe.getRegions()) {
