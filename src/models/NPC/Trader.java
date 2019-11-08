c4509d7b9cb1a1cce3a915850afac856f20366c7
Philippe Desrosiers <phildesro@yahoo.com>
2019-11-07 23:15:21 -0500

    Adding docs to classes

--- src/models/NPC/Trader.java
+++ src/models/NPC/Trader.java
@@ -22,6 +22,11 @@ public class Trader extends NPC {
         angry = false;
     }
 
+    /**
+     * Player can rob trader
+     * @param val
+     * @return
+     */
     public int commitRobbery(int val) {
         if (val > 60) {
             //Random number between 1-3 items
@@ -33,18 +38,37 @@ public class Trader extends NPC {
         }
         //return number
     }
+
+    /**
+     * Changes the state of trader's anger
+     * True if they are angry, False if they are not angry
+     * @param angry Whether NPC is angry or not. True if they are false if they aren't
+     */
     public void setAngry(boolean angry) {
         this.angry = angry;
     }
 
+    /**
+     * Set the cargo of trader
+     * @param cargo Cargo for trader
+     */
     public void setCargo(ArrayList<Item> cargo) {
         this.cargo = cargo;
     }
 
+    /**
+     * Get the player that the NPC will interact with
+     * @return Player that NPC is interacting with
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Finds item to remove from cargo
+     * @param item Item to remove from NPC cargo
+     * @return Item that was removed
+     */
     public Item findItemToRemove(Item item) {
         if (cargo.indexOf(item) > -1) {
             return item;
